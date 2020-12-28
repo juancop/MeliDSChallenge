635f0e3f3f178eccac183db277af7cb161b4fca6
Juan <jcobapuerto@gmail.com>
2020-12-28 06:15:05 +0000

    Se cambia el código de búsqueda de parámetros

--- notebooks/model/xgb_model.py
+++ notebooks/model/xgb_model.py
@@ -1,44 +1,19 @@
-import xgboost as xgb
+from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
 from xgboost import XGBClassifier
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.model_selection import KFold
-from scipy import stats
 
-def define_xgb_for_cv(num_class, scoring = 'f1_macro'):
-    """
-    XGB Initial parameter distribution
-    
-    """
-    param_dist = {'n_estimators': stats.randint(150, 1000),
-              'learning_rate': stats.uniform(0.01, 0.07),
-              'subsample': stats.uniform(0.3, 0.7),
-              'max_depth': [3, 4, 5, 6, 7, 8, 9],
-              'colsample_bytree': stats.uniform(0.5, 0.45),
-              'min_child_weight': [1, 2, 3]
-             }
+def GridSearchXGBClassifier(num_class):
+    XGB = XGBClassifier(objective='multi:softmax', num_class = num_class)
+    xgb_params  = [
+        {    
+        "n_estimators": range(200, 1000, 100),
+        }
+    ]
 
-    kfold_3 = KFold(n_splits = 3, shuffle = True, random_state=42)
+    cv = StratifiedShuffleSplit(n_splits=5, test_size=0.3, random_state=42)
+
+    XGB_to_fit = GridSearchCV(XGB, xgb_params, scoring='f1_macro', cv=cv, n_jobs=-1, verbose=3)
+    return XGB_to_fit
 
 
-    model = XGBClassifier(n_estimators=100,
-                         reg_lambda=1,
-                         gamma=0,
-                         max_depth=3,
-                         objective = 'multi:softmax',
-                         n_jobs = 2,
-                         num_class = num_class)
 
-    model_cv = RandomizedSearchCV(model, 
-                             param_distributions = param_dist,
-                             cv = kfold_3,  
-                             n_iter = 5, 
-                             scoring = scoring, 
-                             error_score = 0, 
-                             verbose = 1, 
-                             n_jobs = -1)
-    
-    return model_cv
-
-## XGB_model = define_xgb_for_cv(num_class, 'f1_macro')
-## XGB_model.fit(X_train, y_train)
-## XGB_model.best_params_
+    Model_1.fit(X_train, y_train)
